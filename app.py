2fa9fd79157b0c82ab56b0e5aa8cb628a3c6d680
kaushikishuklawork <kaushikishuklawork@gmail.com>
2025-09-27 10:38:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import joblib
 import altair as alt
 
 # Load trained models
-models = joblib.load("carbon_model.pkl")
+models = joblib.load("carbon emission_corrected.pkl")
 reg_model = models['regression']
 kmeans_model = models['clustering']
 preprocessor = models['preprocessor']
@@ -66,3 +66,4 @@ if st.button("Predict Carbon Emission & Cluster"):
     )
 
     st.altair_chart(chart, use_container_width=True)
+
